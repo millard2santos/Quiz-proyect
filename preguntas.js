471d81ec8e0905217c0b71c84d08a79d17cbbaee
rochamiguel1 <miroga2005@gmail.com>
2023-01-23 13:23:03 +0100

    resRight

--- preguntas.js
+++ preguntas.js
@@ -8,56 +8,74 @@ const questions = document.querySelector("h2")
 const answers = document.querySelectorAll("article")
 const btnNext = document.querySelector("button")
 
+let resRight = 0;
+let blockAnswer = false
+
+
 // ingresa la respuesta correcta en un array en forma ordenada alfabeticamente
 const imprimirPregunta = (res) => {
+    
     res[0].incorrectAnswers.push(res[0].correctAnswer)
     let arrayAll = res[0].incorrectAnswers.sort()
 
-// muestra la pregunta en el H2
+    // muestra la pregunta en el H2
     questions.innerText = res[0].question
     answers.forEach((e, i) => {
         e.innerText = arrayAll[i]
-        
+
     });
     console.log(res[0].correctAnswer);
 
 
-  let puntos = 0;
-  let maxPuntos = 10;
 
-    //   for each en article y cada vez que se seleccion suma punto
-    answers.forEach((alternativa, ) => {
+    //   for each en article y cada vez que se seleccion 
+    answers.forEach((alternativa) => {
         alternativa.addEventListener("click", event => {
-           
-            
+            console.log(alternativa.innerText, res[0].correctAnswer, resRight, blockAnswer);
+            if (alternativa.innerText === res[0].correctAnswer && blockAnswer === false) {
+                console.log('yes');
+                resRight++
+                console.log(resRight);
+            }
+            blockAnswer = true
+            localStorage.setItem("resRight", resRight)
             answers.forEach(e => {
 
-                if (e.innerText === res[0].correctAnswer && puntos < maxPuntos) {
+                if (e.innerText === res[0].correctAnswer) {
                     e.style = "background-color: blue"
-                    puntos++
+
                 } else {
                     e.style = "background-color: red"
                 }
-                localStorage.setItem("puntos", JSON.stringify(puntos))
+
+
+
             })
 
-            const showbtn = () => {
-                btnNext.style.display = "block";
-                
-            }
-            showbtn()
+            btnNext.style.display = "block"
         })
     });
 
-  
 }
+
+
+
+
+
+
+
 
 // funcion que muestra la pregunta 
 const recogerPregunta = () => {
+    answers.forEach(e => {                     // reinicia las alternativas en la siguiente pregunta
+        e.style = "background-color: white"
+    })
 
-    btnNext.style.display= "none"
+    btnNext.style.display = "none"             // el boton no esta aparece cuando se muestra las alternativas
     return fetch(`https://the-trivia-api.com/api/questions?categories=${user.categoria}&limit=1`)
         .then(response => response.json())
+
+
 
 }
 
@@ -68,7 +86,8 @@ recogerPregunta()
     })
 
 btnNext.addEventListener("click", () => {
-    recogerPregunta()
+        blockAnswer = false
+        recogerPregunta()
         .then(data => {
             imprimirPregunta(data)
 
